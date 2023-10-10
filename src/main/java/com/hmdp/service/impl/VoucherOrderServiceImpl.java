b738153cc1a436c4cb9b4424dc97e4f58ca5fbad
gallantsa <gallantsa@163.com>
2023-10-10 15:35:01 +0800

    优惠券秒杀_乐观锁解决超卖

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -58,8 +58,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
         // 5. 扣减库存
         boolean success = seckillVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId)
+                .setSql("stock = stock - 1") // set stock = stock - 1
+                .eq("voucher_id", voucherId).gt("stock", 0) // where id =? and stock > 0
                 .update();
 
         if (!success) {
