5b8659b04bb100ca024c34880b141e0a9097fa93
gallantsa <gallantsa@163.com>
2023-10-13 09:34:05 +0800

    达人探店_发布探店笔记

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -13,7 +13,7 @@ public class RedissonConfig {
     public RedissonClient redissonClient(){
         // 配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.10.134:6379")
+        config.useSingleServer().setAddress("redis://192.168.10.135:6379")
             .setPassword("123321");
         // 创建RedissonClient对象
         return Redisson.create(config);
