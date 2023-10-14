fc9d974d7e436adf4dd79a54a874188ad94e7779
gallantsa <gallantsa@163.com>
2023-10-14 12:22:58 +0800

    附近商铺_实现附近商户功能_上次提交的测试已更新

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -68,27 +68,25 @@ class HmDianPingApplicationTests {
 
     @Test
     void loadShopData() {
-        // 1. 查询店铺信息
+        // 1.查询店铺信息
         List<Shop> list = shopService.list();
-
-        // 2. 把店铺分组, 按照typeId分组, id一致的放到一个集合
+        // 2.把店铺分组，按照typeId分组，typeId一致的放到一个集合
         Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
-
-        // 3. 分批完成写入Redis
+        // 3.分批完成写入Redis
         for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
-            // 3.1 获取类型id
-            List<Shop> typeId = entry.getValue();
+            // 3.1.获取类型id
+            Long typeId = entry.getKey();
             String key = SHOP_GEO_KEY + typeId;
-            // 3.2 获取同类型的店铺集合
+            // 3.2.获取同类型的店铺的集合
             List<Shop> value = entry.getValue();
-            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>();
-            // 3.3 写入Redis GEOADD key 精度 纬度 member
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            // 3.3.写入redis GEOADD key 经度 纬度 member
             for (Shop shop : value) {
-                // stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString
-                // ());
+                // stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
                 locations.add(new RedisGeoCommands.GeoLocation<>(
                         shop.getId().toString(),
-                        new Point(shop.getX(), shop.getY())));
+                        new Point(shop.getX(), shop.getY())
+                ));
             }
             stringRedisTemplate.opsForGeo().add(key, locations);
         }
