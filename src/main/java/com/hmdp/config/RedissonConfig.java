020efb9b07b0cf72f0c46eb0ebb5fc9eb9876f2b
gallantsa <gallantsa@163.com>
2025-02-20 13:34:46 +0800

    从yml读取redis配置

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -1,19 +1,27 @@
 package com.hmdp.config;
 
+import lombok.Data;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
+@ConfigurationProperties(prefix = "spring.redis")
 public class RedissonConfig {
+
+    private String host;
+    private String port;
+    private int database;
 
     @Bean
     public RedissonClient redissonClient(){
         // 配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(10);
+        config.useSingleServer().setAddress("redis://" + host + ":" + port).setDatabase(database);
         // 创建RedissonClient对象
         return Redisson.create(config);
     }
