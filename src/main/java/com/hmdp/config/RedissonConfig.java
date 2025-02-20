1c0532c451d496e468003b7846aac329dcbcabba
gallantsa <gallantsa@163.com>
2025-02-20 09:13:32 +0800

    取消redis密码

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -13,8 +13,7 @@ public class RedissonConfig {
     public RedissonClient redissonClient(){
         // 配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379")
-            .setPassword("hsp");
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(10);
         // 创建RedissonClient对象
         return Redisson.create(config);
     }
